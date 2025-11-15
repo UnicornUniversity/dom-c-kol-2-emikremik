f9208d5307ff55f54b701d7053acb8de28047281
emikremik <hirschneremik@gmail.com>
2025-11-15 12:39:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,50 +4,56 @@
 /**
  * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
  * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
+ * Program konvertujúci číslo v desinkovej číselnej sústave do Rímskej číselnej sústavy
  * @param {string} inputNumber number that is being converted
  * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
  * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
  * @returns {string} containing number converted to output system
  */
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
-	if (inputNumberSystem !== 10 || outputNumberSystem !== 1) {
-		return "Nepsrávna konverzia"
-	}
+  if (inputNumberSystem !== 10 || outputNumberSystem !== 1) {
+    return "Nepsrávna konverzia";
+  }
 
-	for(let c of inputNumber){
-		if (c<'0'|| c > '9'){
-			return "Číslo musí byť celé číslo väčšie ako 0"
-		}
-	}
+  // Kontrola, ci je cislo cele
+  for (let cislica of inputNumber) {
+    if (cislica < '0' || cislica > '9') {
+      return "Číslo musí byť celé číslo väčšie ako 0";
+    }
+  }
 
-	let n = 0;
-	for (let c of inputNumber) {
-		n = n * 10 + (c.charCodeAt(0) - 48);
-	}
+  // Konvertuje string na integer
+  let cislo = 0;
+  for (let cislica of inputNumber) {
+    cislo = cislo * 10 + (cislica.charCodeAt(0) - 48);
+  }
 
-	if (n<1) {
-		return "Číslo musí byť celé číslo väčšie ako 0"
-	}
+  // Kontroluje ci je cislo vacsie ako 0
+  if (cislo < 1) {
+    return "Číslo musí byť celé číslo väčšie ako 0"; 
+  }
 
-	cont values = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-	cont numerals = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+  // Definuje rimske cislice a ich korespondujuce desinkove cisla
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
-	let result = "";
-	let i = 0;
+  let result = "";
 
-	while (n>0) {
-		if (n >= values[i]){
-			result *- numerals[i];
-			n -= valies[i];
-		} else {
-			i++
-		}
-	}
-	return result;
+  // Hlavna konverzia desinkoveho na rimske cislo
+  let RimskeCislo = 0;
+  while (cislo > 0) {
+    if (cislo >= values[RimskeCislo]) {
+      result += numerals[RimskeCislo]; // Prida rimsku cislicu k vysledku
+      cislo -= values[RimskeCislo]; // odcita hodnotu od cisla
+    } else {
+      RimskeCislo++; 
+    }
+  }
+
+  // Vysledne rimske cislo a vysledok v konzole
+	console.log(inputNumber + " je v Rímskej číselnej zostave " + result);
+  	return result;
 }
-  //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
 /**
  * TODO - Change this to contain all input number systems that your application can convert from.
  * Function which returns which number systems are permitted on input.
